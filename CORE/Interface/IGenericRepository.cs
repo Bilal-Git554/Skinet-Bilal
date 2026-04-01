c57e6a8f7878423c40b2aa444ede43d864a27b07
Bilal-Git554 <bk2155453@gmail.com>
2026-04-01 16:42:29 +0530

    Implemented The IGenericRepository In The GenericRepository...

--- CORE/Interface/IGenericRepository.cs
+++ CORE/Interface/IGenericRepository.cs
@@ -7,9 +7,9 @@ public interface IGenericRepository<T> where T : BaseEntity
 {
   Task<T?> GetByIdAsync(int id);
   Task<IReadOnlyList<T>> ListAllAsync();
-  void AddAsync(T entity);
-  void UpdateAsync(T entity);
-  void DeleteAsync(T entity);
+  void Add(T entity);
+  void Update(T entity);
+  void Remove(T entity);
   Task<bool> SaveAllAsync();
   bool Exists(int id);
 
--- INFRASTRUCTURE/Datas/GenericRepository.cs
+++ INFRASTRUCTURE/Datas/GenericRepository.cs
@@ -1,6 +1,7 @@
 using System;
 using CORE.Entities;
 using CORE.Interface;
+using Microsoft.EntityFrameworkCore;
 namespace INFRASTRUCTURE.Datas;
 
 public class GenericRepository<T> : IGenericRepository<T> where T : BaseEntity
@@ -11,38 +12,39 @@ public class GenericRepository<T> : IGenericRepository<T> where T : BaseEntity
         _context = context;
     }
     //DB Connection
-    public void AddAsync(T entity)
+    public void Add(T entity)
     {
-        throw new NotImplementedException();
+        _context.Set<T>().Add(entity);
     }
 
-    public void DeleteAsync(T entity)
+    public void Remove(T entity)
     {
-        throw new NotImplementedException();
+        _context.Set<T>().Remove(entity);
     }
 
     public bool Exists(int id)
     {
-        throw new NotImplementedException();
+        return _context.Set<T>().Any(x =>x.Id == id);
     }
 
-    public Task<T?> GetByIdAsync(int id)
+    public async Task<T?> GetByIdAsync(int id)
     {
-        throw new NotImplementedException();
+        return await _context.Set<T>().FindAsync(id);
     }
 
-    public Task<IReadOnlyList<T>> ListAllAsync()
+    public async Task<IReadOnlyList<T>> ListAllAsync()
     {
-        throw new NotImplementedException();
+        return await _context.Set<T>().ToListAsync();
     }
 
-    public Task<bool> SaveAllAsync()
+    public async Task<bool> SaveAllAsync()
     {
-        throw new NotImplementedException();
+        return await _context.SaveChangesAsync() > 0;
     }
 
-    public void UpdateAsync(T entity)
+    public void Update(T entity)
     {
-        throw new NotImplementedException();
+        _context.Set<T>().Attach(entity);
+        _context.Entry(entity).State=EntityState.Modified;
     }
 }
